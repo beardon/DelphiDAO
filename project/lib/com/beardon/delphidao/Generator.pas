4538658b40be871c04937e3fbe9ff76dd5ca7115
aaronbean <aaron.bean@beardon.com>
2011-10-04 17:10:17 +0000

    adding Interface def for QueryByLike

--- project/lib/com/beardon/delphidao/Generator.pas
+++ project/lib/com/beardon/delphidao/Generator.pas
@@ -506,6 +506,8 @@ begin
         else
         begin
           queryByDef := queryByDef + TAB2 + 'function QueryBy' + fieldMemberName + '(const Value: ' + delphiType + '; const IsLike: Boolean): TObjectList<T' + tableClassName + '>;' + CRLF;
+          if (delphiType = 'string') then
+            queryByDef := queryByDef + TAB2 + 'function QueryByLike' + fieldMemberName + '(const Value: ' + delphiType + '; const IsLike: Boolean): TObjectList<T' + tableClassName + '>;' + CRLF;
           deleteByDef := deleteByDef + TAB2 + 'function DeleteBy' + fieldMemberName + '(const Value: ' + delphiType + '): Integer;' + CRLF;
         end;
         Next;
